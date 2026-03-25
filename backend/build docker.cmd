0a01774a09f1d044f81cc5462a53cd2729ef27cd
COL-8283-LI05 <luisaugustodesouza785fbi@gmail.com>
2026-03-24 21:20:47 -0300

    updated env

--- backend/build docker.cmd
+++ backend/build docker.cmd
@@ -11,7 +11,7 @@ echo.
 docker build -f Dockerfile.windows -t backend .
 docker run -d -p 8000:8000 ^
 --name backend_%computername% ^
--e DATABASE_URL=postgres://%DATABASE_USER%:%DATABASE_PASSWORD%@192.168.6.100:%DATABASE_PORT%/meubanco ^
+-e DATABASE_URL=postgres://%DATABASE_USER%:%DATABASE_PASSWORD%@192.168.6.100:%DATABASE_PORT%/ifesdoc ^
 backend
 docker ps
 timeout 10
