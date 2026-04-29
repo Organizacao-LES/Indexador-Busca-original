32ff8581ab2438f826e2627bb06dfa39d31abbaa
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-04-29 10:51:46 -0300

    updated

--- backend/build docker.cmd
+++ backend/build docker.cmd
@@ -9,10 +9,6 @@ echo DATABASE_URL="postgres://%DATABASE_USER%:%DATABASE_PASSWORD%@%DATABASE_HOST
 echo ==========================
 echo.
 
-docker build -f Dockerfile.windows -t backend .
-docker run -d -p 8000:8000 ^
---name backend_%computername% ^
--e DATABASE_URL=postgresql://%DATABASE_USER%:%DATABASE_PASSWORD%@%DATABASE_HOST%:%DATABASE_PORT%/ifesdoc ^
-backend
-docker ps
+@REM docker build -f Dockerfile.windows -t backend .
+run-docker.cmd
 timeout 10
---
+++ interface-web/run-docker.cmd
@@ -0,0 +1,4 @@
+@echo off
+ECHO RUN MODE
+docker run -d -p 8080:8080 --name frontend_%computername% frontend
+docker ps
